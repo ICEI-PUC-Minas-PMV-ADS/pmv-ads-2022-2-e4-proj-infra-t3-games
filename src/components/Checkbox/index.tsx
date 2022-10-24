234a8e570a30fff4eb8559b9ef066c559a674af4
Jean-Stefan <jeanss12@gmail.com>
2022-10-24 16:27:15 -0300

    chore: adicionado configs eslint, prettier

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -1,45 +1,45 @@
-import React from 'react'
-import { InputHTMLAttributes, useState } from 'react'
-import * as S from './styles'
+import React, { useEffect } from 'react';
+import { InputHTMLAttributes, useState } from 'react';
+import * as S from './styles';
 
 export type CheckboxProps = {
-  onCheck?: (status: boolean) => void
-  isChecked?: boolean
-  label?: string
-  labelFor?: string
-  value?: string | ReadonlyArray<string> | number
-} & InputHTMLAttributes<HTMLInputElement>
+    onCheck?: (status: boolean) => void;
+    isChecked?: boolean;
+    label?: string;
+    labelFor?: string;
+    value?: string | ReadonlyArray<string> | number;
+} & InputHTMLAttributes<HTMLInputElement>;
 
 const Checkbox = ({
-  onCheck,
-  isChecked = false,
-  label,
-  labelFor = '',
-  value,
-  ...props
+    onCheck,
+    isChecked = false,
+    label,
+    labelFor = '',
+    value,
+    ...props
 }: CheckboxProps) => {
-  const [checked, setChecked] = useState(isChecked)
+    const [checked, setChecked] = useState(isChecked);
 
-  const onChange = () => {
-    const status = !checked
-    setChecked(status)
+    const onChange = () => {
+        const status = !checked;
+        setChecked(status);
 
-    !!onCheck && onCheck(status)
-  }
+        !!onCheck && onCheck(status);
+    };
 
-  return (
-    <S.Wrapper>
-      <S.Input
-        id={labelFor}
-        type="checkbox"
-        onChange={onChange}
-        checked={checked}
-        value={value}
-        {...props}
-      />
-      {!!label && <S.Label htmlFor={labelFor}>{label}</S.Label>}
-    </S.Wrapper>
-  )
-}
+    return (
+        <S.Wrapper>
+            <S.Input
+                id={labelFor}
+                type='checkbox'
+                onChange={onChange}
+                checked={checked}
+                value={value}
+                {...props}
+            />
+            {!!label && <S.Label htmlFor={labelFor}>{label}</S.Label>}
+        </S.Wrapper>
+    );
+};
 
-export default Checkbox
+export default Checkbox;
