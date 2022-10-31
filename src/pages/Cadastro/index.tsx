a44114c8aefef459eccdbacda3cf92802fa04c42
Jean-Stefan <jeanss12@gmail.com>
2022-10-31 16:47:28 -0300

    feat: adicionado yup

--- src/pages/Cadastro/index.tsx
+++ src/pages/Cadastro/index.tsx
@@ -4,9 +4,26 @@ import Input from '../../components/Input';
 import Title from '../../components/Title';
 import { CadastroWrapper, Form } from './style';
 import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+
+interface FormData {
+    email: string;
+    password: string;
+    confirm: string;
+}
+
+const schema = yup.object({
+    email: yup
+        .string()
+        .required('Digite seu e-mail.')
+        .matches(
+            /^[a-z0-9._-]+(?:\.[a-z0-9._-]+)*@(?:[a-z0-9](?:[a-z-]*[a-z])?.)+[a-z](?:[a-z]*[a-z]){1,}?$/,
+            'Email inválido',
+        ),
+});
 
 const Cadastro = () => {
-    const { register, getValues } = useForm();
+    const { register, getValues } = useForm<FormData>();
     return (
         <CadastroWrapper>
             <Title>Cadastre-se</Title>
@@ -16,7 +33,7 @@ const Cadastro = () => {
                 <Input
                     placeholder='Confirmação de senha'
                     type='password'
-                    {...register('confirmation')}
+                    {...register('confirm')}
                 />
                 <Button>Cadastrar</Button>
             </Form>
