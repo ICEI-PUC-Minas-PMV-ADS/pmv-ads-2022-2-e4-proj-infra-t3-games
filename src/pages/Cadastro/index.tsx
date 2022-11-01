25b8f515ab80b6506d6d5eb9dafebe4ce400a685
Jean-Stefan <jeanss12@gmail.com>
2022-10-31 21:50:28 -0300

    feat: regex senha atualizado

--- src/pages/Cadastro/index.tsx
+++ src/pages/Cadastro/index.tsx
@@ -21,7 +21,13 @@ const schema = yup.object({
             /^[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/,
             'Email inválido',
         ),
-    password: yup.string().required('Digite uma senha'),
+    password: yup
+        .string()
+        .required('Digite uma senha')
+        .matches(
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#^])[A-Za-z\d@$!%*?&#^]{8,}$/,
+            'A senha deve ter no mínimo 8 caracteres sendo uma letra maiúscula, uma minúscula, um número e um caractere especial',
+        ),
     confirm: yup
         .string()
         .required('Digite a confirmaçao de senha')
