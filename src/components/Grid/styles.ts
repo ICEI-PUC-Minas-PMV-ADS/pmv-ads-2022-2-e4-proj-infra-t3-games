a8c7d6003546bede371ecc6010f2f231582a5307
Jean-Stefan <jeanss12@gmail.com>
2022-11-01 21:48:38 -0300

    feat: componente grid criado

---
+++ src/components/Grid/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const StyledGrid = styled.div`
+    display: grid;
+    grid-template-columns: 1fr;
+    min-height: 100vh;
+    width: 100%;
+    background: #211b23;
+    padding-bottom: 45px;
+`;
