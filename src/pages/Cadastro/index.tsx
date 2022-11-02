6ffbaba1e2f8fdfd3bd9b8a6275ac94d310fb4cc
Jean-Stefan <jeanss12@gmail.com>
2022-11-01 21:56:30 -0300

    refactor: mudança na estrutura da pág cadastro

--- src/pages/Cadastro/index.tsx
+++ src/pages/Cadastro/index.tsx
@@ -2,11 +2,14 @@ import Button from '../../components/Button';
 import FooterBanner from '../../components/FooterBanner';
 import Input from '../../components/Input';
 import Title from '../../components/Title';
-import { CadastroWrapper, Form } from './style';
 import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from 'yup';
 import { CognitoUserPool } from 'amazon-cognito-identity-js';
+import Navbar from '../../components/Navbar';
+import Grid from '../../components/Grid';
+import Form from '../../components/Form';
+import Footer from '../../components/Footer';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { schemaCadastro } from '../../utils/yupSchema';
 
 interface FormData {
     email: string;
@@ -14,26 +17,7 @@ interface FormData {
     confirm: string;
 }
 
-const schema = yup.object({
-    email: yup
-        .string()
-        .required('Digite seu e-mail')
-        .matches(
-            /^[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/,
-            'Email inválido',
-        ),
-    password: yup
-        .string()
-        .required('Digite uma senha')
-        .matches(
-            /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#^])[A-Za-z\d@$!%*?&#^]{8,}$/,
-            'A senha deve ter no mínimo 8 caracteres sendo uma letra maiúscula, uma minúscula, um número e um caractere especial',
-        ),
-    confirm: yup
-        .string()
-        .required('Digite a confirmaçao de senha')
-        .oneOf([yup.ref('password'), null], 'As senhas devem ser iguais'),
-});
+const items = [{ name: 'Iniciar Sessão', link: '/login' }];
 
 const Cadastro = () => {
     const {
@@ -42,12 +26,11 @@ const Cadastro = () => {
         handleSubmit,
         formState: { errors },
     } = useForm<FormData>({
-        resolver: yupResolver(schema),
+        resolver: yupResolver(schemaCadastro),
     });
 
     const onSubmit = (data: FormData) => {
         const userPool = new CognitoUserPool({
-
             UserPoolId: `${process.env.REACT_APP_USER_POOL_ID}`,
             ClientId: `${process.env.REACT_APP_CLIENT_ID}`,
         });
@@ -58,12 +41,14 @@ const Cadastro = () => {
 
             //TODO
             //navegar para validarcode-page se cadastro for bem-sucedido
-
         });
     };
 
     return (
-        <CadastroWrapper>
+        <Grid>
+            <header>
+                <Navbar items={items} />
+            </header>
             <Title>Cadastre-se</Title>
             <Form onSubmit={handleSubmit(onSubmit)}>
                 <Input
@@ -86,8 +71,10 @@ const Cadastro = () => {
                 />
                 <Button type='submit'>Cadastrar</Button>
             </Form>
-            <FooterBanner />
-        </CadastroWrapper>
+            <Footer>
+                <FooterBanner />
+            </Footer>
+        </Grid>
     );
 };
 
