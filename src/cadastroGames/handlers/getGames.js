07b968c5541a7ecc689a9004d5de87775cbd0618
Jean-Stefan <jeanss12@gmail.com>
2022-11-04 14:42:06 -0300

    fix: ajuste de cors nas funções game

--- src/cadastroGames/handlers/getGames.js
+++ src/cadastroGames/handlers/getGames.js
@@ -1,14 +1,18 @@
 const serverless = require('serverless-http');
 const express = require('express');
-const cors = require('cors');
 const {PrismaClient} = require('@prisma/client');
 
 const prisma = new PrismaClient();
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+    next();
+});
+
 app.use(express.json());
-app.use(cors());
 
 app.get('/games', async (req, res) => {
     const games = await prisma.games.findMany({});
