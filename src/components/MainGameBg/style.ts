0e95db7fa783c35f1b366d131271ec8d333704a5
Jean-Stefan <jeanss12@gmail.com>
2022-11-05 12:37:47 -0300

    feat: adicionado componente bg jogo principal

---
+++ src/components/MainGameBg/style.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+import cod from '../../assets/img/cod.jpg';
+
+export const Background = styled.div`
+    background-image: url(${cod});
+    height: 100vh;
+    position: absolute;
+    left: 0;
+    right: 0;
+    z-index: -1;
+    filter: blur(8px);
+    -webkit-filter: blur(8px);
+    opacity: 0.6;
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+`;
