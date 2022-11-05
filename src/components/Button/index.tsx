919b6ea4541d7cab316413ee425ecc454dbc970a
Jean-Stefan <jeanss12@gmail.com>
2022-11-05 12:32:15 -0300

    refactor: renomeando styles.ts

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,5 +1,5 @@
 import { ComponentPropsWithoutRef } from 'react';
-import StyledButton from './styles';
+import StyledButton from './style';
 
 const Button = ({ children, ...props }: ComponentPropsWithoutRef<'button'>) => {
     return <StyledButton {...props}>{children}</StyledButton>;
