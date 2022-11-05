7f504701131791ba8ef099e4e4855635a26f536d
Jean-Stefan <jeanss12@gmail.com>
2022-11-05 12:30:15 -0300

    refactor: removido imports desnecessários

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { GlobalStyles } from "./styles/global";
-import { ThemeProvider } from "styled-components";
-import theme from "./styles/theme";
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { GlobalStyles } from './styles/global';
+import { ThemeProvider } from 'styled-components';
+import theme from './styles/theme';
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
 root.render(
-  <>
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <App />
-    </ThemeProvider>
-  </>
+    <>
+        <ThemeProvider theme={theme}>
+            <GlobalStyles />
+            <App />
+        </ThemeProvider>
+    </>,
 );
