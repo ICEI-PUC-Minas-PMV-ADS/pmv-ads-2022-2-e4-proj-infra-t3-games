fa7d08a307aae32964b7898cb34a6e495c166da2
Jean-Stefan <jeanss12@gmail.com>
2022-11-05 14:28:34 -0300

    style: adicionado estilo da loja

--- src/pages/Loja/style.ts
+++ src/pages/Loja/style.ts
@@ -1,1 +1,40 @@
 import styled from 'styled-components';
+
+export const Main = styled.main`
+    max-width: 1064px;
+    margin: 0 auto;
+    padding: 0 20px;
+`;
+
+export const MainGameGroup = styled.div`
+    margin-top: 168px;
+    margin-bottom: 100px;
+`;
+
+export const MainGameDescription = styled.p`
+    max-width: 456px;
+    color: #ffffff;
+    margin-bottom: 26px;
+    line-height: 24px;
+`;
+
+export const LancamentosGroup = styled.div`
+    margin-bottom: 100px;
+`;
+
+export const LancamentosTitle = styled.div`
+    margin-bottom: 50px;
+`;
+
+export const LancamentosText = styled.p`
+    width: 5ch;
+    font-size: 16px;
+    font-weight: 400;
+    color: #ffffff;
+`;
+
+export const LancamentosSeparator = styled.p`
+    width: 130px;
+    margin: 5px 0;
+    border: 1px solid #bd0059;
+`;
