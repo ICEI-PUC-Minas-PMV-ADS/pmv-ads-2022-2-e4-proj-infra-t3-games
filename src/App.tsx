204063b2a1278f2c8d7b95f95b88927b020559ae
Jean-Stefan <jeanss12@gmail.com>
2022-11-17 16:46:53 -0300

    feat: added game route

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,19 @@ import ValidarCode from './pages/ValidarCode';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Loja from './pages/Loja';
 import CadastroGame from './pages/CadastroGame';
+import Game from './pages/Game';
 
 function App() {
     return (
         <div className='App'>
             <Router>
                 <Routes>
-                    <Route path="*" element={<Navigate to="/loja" />} />
+                    <Route path='*' element={<Navigate to='/loja' />} />
                     <Route path='/cadastro' element={<Cadastro />} />
                     <Route path='/login' element={<Login />} />
                     <Route path='/code' element={<ValidarCode />} />
                     <Route path='/loja' element={<Loja />} />
+                    <Route path='/loja/game/:id' element={<Game />} />
                     <Route path='/cadastrogame' element={<CadastroGame />} />
                 </Routes>
             </Router>
