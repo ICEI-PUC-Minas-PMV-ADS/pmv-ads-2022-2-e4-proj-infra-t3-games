2b0aacbe130fe06405843f61a4031ee5c6c5069b
Jean-Stefan <jeanss12@gmail.com>
2022-11-17 22:48:17 -0300

    feat: adicionado pag biblioteca

---
+++ src/pages/Biblioteca/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from '../../components/Navbar';
+import { Main } from '../Loja/style';
+
+const Biblioteca = () => {
+    return (
+        <div style={{ paddingBottom: '40px' }}>
+            {/* <Navbar></Navbar> */}
+            <Main></Main>;
+        </div>
+    );
+};
+
+export default Biblioteca;
